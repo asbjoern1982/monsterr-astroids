e7f067a68f3780dac570d6fe35445ef9e20974d2
Asbjørn Madsen <asbjoern1982@gmail.com>
2018-09-30 13:37:05 +0200

    added drag to the ships

--- src/stages/astroids/server/model.js
+++ src/stages/astroids/server/model.js
@@ -5,16 +5,23 @@ function Ship (owner) {
   this.velocety = {x: 0, y: 0}
   this.turningSpeed = 0
   this.accelerating = 0
+  this.drag = 0.99
 
   this.update = () => {
+    // move
     this.pos.x += this.velocety.x
     this.pos.y += this.velocety.y
 
+    // turn
     this.heading += this.turningSpeed
+
+    // add accelerate
     this.velocety.x += Math.cos(this.heading) * this.accelerating
     this.velocety.y += Math.sin(this.heading) * this.accelerating
 
-    // TODO apply drag
+    // apply drag
+    this.velocety.x *= this.drag
+    this.velocety.y *= this.drag
   }
 
   this.starteAccelerate = () => {
